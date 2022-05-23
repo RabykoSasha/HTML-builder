1e77417b3a04a95c8a211b3fd09e0b10fa0e4edb
RabykoSasha <56646143+RabykoSasha@users.noreply.github.com>
2022-05-24 01:15:35 +0300

    Init: completed task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const { rm } = require('fs/promises');
+const { mkdir } = require('fs/promises');
+const { copyFile } = require('fs/promises');
+const { readdir } = require('fs/promises');
+const path = require('path');
 
+const oldPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
+
+async function copyDir(fromDir, toDir) {
+    await rm(newPath, { force: true, recursive: true });
+    await mkdir(newPath);
+    const files = await readdir(fromDir, { withFileTypes: true });
+    for (const file of files) {
+        if (file.isDirectory()) continue;
+        const filePath = path.join(fromDir, file.name);
+        const newPath = path.join(toDir, file.name);
+        await copyFile(filePath, newPath);
+    }
+}
+
+copyDir(oldPath, newPath);
